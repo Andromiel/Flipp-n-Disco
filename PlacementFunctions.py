65d366fbe21726e94d1626548675067b3bc36881
Arthur S <arsmadja92@gmail.com>
2024-04-21 20:35:11 +0200

    Added rotation for textures (press directional arrows), removed rotation center offset

--- PlacementFunctions.py
+++ PlacementFunctions.py
@@ -1,18 +1,19 @@
 import pygame
 import time
 
-
+REPOS_OBJECT = 1
+REPOS_ROTATION_CENTER = 2
 class MapObject:
-    def __init__(self, img, posx=None, posy=None, rotationcenter=None):
+    def __init__(self, img, posx=0, posy=0, rotationcenter=None):
+        self.original_img = img
         self.img = img
-        self.pos = (posx, posy)
         self.reposition = 0
         self.rect = img.get_rect()
         self.rect.x, self.rect.y = posx, posy
+        self.originaloffset = pygame.Vector2(0, 0)
         self.mask = pygame.mask.from_surface(self.img)
         self.rotationcenter = self.rect.center
         self.mouseoffset = (0,0)
-        self.centeroffset = (0,0)
         self.angle = 0
         self.rotating = False
 
@@ -20,6 +21,15 @@ class MapObject:
         surface.blit(self.img, self.rect)
         pygame.draw.circle(surface, (255,0,0), self.rotationcenter, 3)
         pygame.draw.line(surface, (0, 255, 255), self.rect.center, self.rotationcenter)
+    def move_at(self, pos):
+        pos = pygame.Vector2(pos[0], pos[1])
+        offset = pygame.Vector2(self.rect.center)
+        offset = self.rotationcenter-offset
+
+        self.rect.center = pygame.Vector2(pos)
+        self.rotationcenter = pos + offset
+
+
 
     def collidesmouse(self):
         mouse_pos = pygame.mouse.get_pos()
@@ -32,24 +42,28 @@ class MapObject:
         self.rect.width = size[0]
         self.img = self.rect.size
 
-    def rotate_around_point(self):
-        rotated_img = pygame.transform.rotate(self.img, 1)
-        origin_vec = pygame.Vector2.from_polar((self.rect.center[0], self.rect.center[1]))
-        pivot_vec = pygame.Vector2.from_polar((self.rotationcenter[0], self.rotationcenter[1]))
-        offset = pivot_vec + (origin_vec - pivot_vec).rotate(-1)
-        self.rect = rotated_img.get_rect(center=offset)
+    def rotate_around_point(self, angle):
+        self.angle+=angle
+        rotated_img = pygame.transform.rotate(self.original_img, self.angle)
+        origin_vec = pygame.Vector2(self.rect.center)
+        pivot_vec = pygame.Vector2(self.rotationcenter)
+        #origin_vec.from_polar((self.rect.center[0], self.rect.center[1]))
+        #pivot_vec.from_polar((self.rotationcenter[0], self.rotationcenter[1]))
+
+        offset = pivot_vec - origin_vec
+        new_center = pivot_vec - self.originaloffset.rotate(-self.angle)
+        self.rect = rotated_img.get_rect(center=new_center)
+        self.img = rotated_img
+        self.mask = pygame.mask.from_surface(self.img)
 
     def update(self, surface):
-        if self.reposition == 1:
-            mouse_pos = pygame.mouse.get_pos()
-            self.rect.x = mouse_pos[0]+self.mouseoffset[0]
-            self.rect.y = mouse_pos[1]+self.mouseoffset[1]
-        elif self.reposition == 2:
-            mouse_pos = pygame.mouse.get_pos()
-            self.centeroffset = (self.rect.centerx-mouse_pos[0], self.rect.centery-mouse_pos[1])
-        if self.rotating:
-            self.rotate_around_point()
-        self.rotationcenter = (self.rect.center[0]-self.centeroffset[0], self.rect.center[1]-self.centeroffset[1])
+        mouse_pos = pygame.mouse.get_pos()
+
+        if self.reposition == REPOS_OBJECT:
+            self.move_at(mouse_pos+self.mouseoffset)
+        elif self.reposition == REPOS_ROTATION_CENTER:
+            self.rotationcenter = mouse_pos
+            self.originaloffset = pygame.Vector2(self.rotationcenter)-pygame.Vector2(self.rect.center)
         self.display(surface)
 
 '''
--- Test.py
+++ Test.py
@@ -1,5 +1,3 @@
-import pygame
-import time
 from PlacementFunctions import *
 
 pygame.init()
@@ -7,26 +5,34 @@ pygame.init()
 screen = pygame.display.set_mode((500,600))
 Ball = MapObject(pygame.transform.scale(pygame.image.load("textures/ball.png"), (60,60)), 50,50)
 Disk = MapObject(pygame.transform.scale(pygame.image.load("textures/disk.png"), (60,60)), 0,0)
-Flipper = MapObject(pygame.transform.scale(pygame.image.load("textures/flipper.png"), (100,60)), 90,90)
+Flipper = MapObject(pygame.transform.scale(pygame.image.load("textures/play_button.png"), (100,100)), 90,90)
 Objects = [Disk, Ball, Flipper]
 loop = True
 angle = 360
 count = 0
+test_vec = pygame.Vector2((0, 1))
+test_vec=test_vec.rotate(90)
+print(test_vec)
 while loop:
     screen.fill((30, 90, 120))
+    pygame.draw.line(screen, (200, 0, 0), test_vec, test_vec+test_vec, 3)
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             loop = False
         for object in Objects:
             if event.type == pygame.MOUSEBUTTONDOWN and object.collidesmouse():
-                if event.button == 1:
-                    object.reposition = 1
-                elif event.button == 3:
-                    object.reposition = 2
+                if event.button == pygame.BUTTON_LEFT:
+                    object.reposition = REPOS_OBJECT
+                elif event.button == pygame.BUTTON_RIGHT:
+                    object.reposition = REPOS_ROTATION_CENTER
             elif event.type == pygame.MOUSEBUTTONUP:
                 object.reposition = 0
-            elif event.type == pygame.KEYDOWN:
-                object.rotate_around_point()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    object.rotate_around_point(10)
+                elif event.key == pygame.K_DOWN:
+                    object.rotate_around_point(-10)
 
     Ball.update(screen)
     Disk.update(screen)
